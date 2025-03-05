f42686f8b5416ffaff294dfa5204ac9bd4a1e3bc
vethacagdas <vethanurcagdas@gmail.com>
2025-03-05 14:32:49 +0300

    Add project files.

---
+++ BookRentalWithoutDB/Data/StudentRespository.cs
@@ -0,0 +1,28 @@
+﻿using BookRentalWithoutDB.Models;
+using System.Linq; // Where ve FirstOrDefault kullanımı için eklendi.
+using System.Collections.Generic; // List için eklendi.
+using System; // DateTime için eklendi.
+
+namespace BookRentalWithoutDB.Data
+{
+    public class StudentRespository
+    {
+        static List<Student> data = new List<Student>
+        {
+            new Student { ID = 1, Name = "Hüseyin", Surname = "Şimşek", BirthDate = new DateTime(1990, 1, 1) },
+            new Student { ID = 2, Name = "Mehmet", Surname = "Yıldız", BirthDate = new DateTime(2001, 1, 1) },
+            new Student { ID = 4, Name = "Ayşe", Surname = "Kaya", BirthDate = new DateTime(2004, 1, 1) }
+        };
+
+        public List<Student> GetAllStudents()
+        {
+            return data; // "Select * from Students";
+        }
+
+        public Student GetStudent(int id)
+        {
+            Student result = data.Where(s => s.ID == id).FirstOrDefault();
+            return result;
+        }
+    }
+}
---
+++ BookRentalWithoutDB/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace BookRentalWithoutDB.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime BirthDate {get;set;}
+    }
+}
